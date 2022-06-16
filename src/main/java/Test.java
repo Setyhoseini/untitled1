6f993652140bf0dd347ea25e1e1211cfe6b9f9ec
parsa__sbr <parsa.sabrian22@gmail.com>
2022-06-16 21:04:14 +0430

    parsa's second commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,13 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("hey");
+
+
+
+
+
+
+
+        System.out.println("you good??");
     }
 }
